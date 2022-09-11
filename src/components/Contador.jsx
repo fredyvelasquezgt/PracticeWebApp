f755d46690085834113240a57c34f27b8f4982a1
Fredy Velasquez <macacoloco696@gmail.com>
2022-09-10 22:33:56 -0600

    Ternary added

--- src/components/Contador.jsx
+++ src/components/Contador.jsx
@@ -13,6 +13,11 @@ const Contador = () => {
         <div>
             <h2>Contador</h2>
             <h3>nuestro numero aumentando: {contador}</h3>
+            <h4>
+                {
+                    contador > 2 ? 'es mayor a 2' : 'es menor a dos'
+                }
+            </h4>
             <button onClick={ () => aumentar()}>Aumentar</button>
         </div>
     )
