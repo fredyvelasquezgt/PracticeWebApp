cd90c09828087252f8f662f66b572602d5e4d865
Fredy Velasquez <macacoloco696@gmail.com>
2022-09-10 23:55:36 -0600

    onChange added to inputs

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,6 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Formulario = () => {
+
+
+    const [fruta, setFruta] = useState('')
+    const [descripcion, setDescripcion] = useState('')
+
+    //hago un estado por cada input
+
     return (
         <div>
             <h2>Formulario</h2>
@@ -8,10 +15,12 @@ const Formulario = () => {
                 <input type="text"
                 placeholder="Ingrese fruta"
                 className="form-control mb-2" 
+                onChange={e => setFruta(e.target.value)}
                 />
                   <input type="text"
                 placeholder="Ingrese descripcion"
                 className="form-control mb-2" 
+                onChange={e => setDescripcion(e.target.value)}
                 />
             </form>
             <button className="btn btn-primary btn-block">Agregar</button>
