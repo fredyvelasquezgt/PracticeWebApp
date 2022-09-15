733580a1aad1d15718f44a236d81c8ee4a216513
Fredy Velasquez <macacoloco696@gmail.com>
2022-09-15 16:26:43 -0600

    Form finished

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -8,22 +8,44 @@ const Formulario = () => {
 
     //hago un estado por cada input
 
+
+    const guardarDatos = (e) => {
+        e.preventDefault() //evito lo que haga por defecto
+
+        if(!fruta.trim()) {
+            console.log('esta vacio fruta')
+            return //el retyrb
+        }
+
+        if(!descripcion.trim()) {
+            console.log('esta vacio descripcion')
+            return
+        }
+    
+        console.log('procesando datos...' + fruta + descripcion)
+
+    }
+
     return (
         <div>
             <h2>Formulario</h2>
-            <form>
-                <input type="text"
-                placeholder="Ingrese fruta"
-                className="form-control mb-2" 
-                onChange={e => setFruta(e.target.value)}
+            <form onSubmit={guardarDatos}>
+                <input 
+                    type="text"
+                    placeholder="Ingrese fruta"
+                    className="form-control mb-2" 
+                    onChange={e => setFruta(e.target.value)}
                 />
-                  <input type="text"
-                placeholder="Ingrese descripcion"
-                className="form-control mb-2" 
-                onChange={e => setDescripcion(e.target.value)}
+                  <input 
+                    type="text"
+                    placeholder="Ingrese descripcion"
+                    className="form-control mb-2" 
+                    onChange={e => setDescripcion(e.target.value)}
                 />
+                    <button className="btn btn-primary btn-block"
+                    type="submit">Agregar</button>
+
             </form>
-            <button className="btn btn-primary btn-block">Agregar</button>
         </div>
     )
 }
